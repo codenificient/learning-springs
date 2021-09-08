2d078556a3c44622312d6d6d212d0498f8a9dacd
CodenificenT User <xristianty@gmail.com>
2021-09-07 20:50:24 -0400

    Initial commit

---
+++ src/test/java/com/codenificient/lil/learningsprings/LearningSpringsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenificient.lil.learningsprings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
